1c4e50dd9373d12dd7ac7f211dc23f79efeb01db
hui-Zz <hui0.0713@gmail.com>
2019-07-06 14:18:25 +0800

    测试

--- hui-boot-api/src/test/java/com/hui/project/web/UserControllerTest.java
+++ hui-boot-api/src/test/java/com/hui/project/web/UserControllerTest.java
@@ -1,49 +1,44 @@
 package com.hui.project.web;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.Assert.assertThat;
 
+import java.net.URL;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.hui.project.base.BaseControllerTest;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-public class UserControllerTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserControllerTest extends BaseControllerTest {
+
+	@LocalServerPort
+	private int port;
+
+	private URL base;
 
 	@Autowired
-	private MockMvc mockMvc;
+	private TestRestTemplate template;
 
-	//@Before
-	//public void setUp() throws Exception {
-	//	mockMvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
-	//}
-
-	//验证controller是否正常响应并打印返回结果
-	@Test
-	public void getAllUser() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/find?id=1").accept(MediaType.APPLICATION_JSON))
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andDo(MockMvcResultHandlers.print())
-				.andReturn();
+	@Before
+	public void setUp() throws Exception {
+		this.base = new URL("http://localhost:" + port + "/");
 	}
 
-	//验证controller是否正常响应并判断返回结果是否正确
 	@Test
 	public void getUser() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("welcome!")));
+		ResponseEntity<String> response = template.getForEntity(base.toString(),
+				String.class);
+		assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
 	}
 
 }
